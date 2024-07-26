41b2eb31a053df75c5e490f825d1b67dc902e418
Sarmad Al Kinany <sarmad.alkinany@tivian.com>
2024-07-26 13:39:39 +0200

    working with OneToMany relation

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\MicroPost;
 use App\Entity\User;
 use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
 use App\Repository\UserProfileRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,8 +23,28 @@ class HelloController extends AbstractController
     ];
 
     #[Route('/', name: 'app_index')]
-    public function index(UserProfileRepository $profiles): Response
+    public function index(MicroPostRepository $posts, CommentRepository $comments): Response
     {
+//         $post = new MicroPost();
+//         $post->setTitle('Hello');
+//         $post->setText('Hello');
+//         $post->setCreated(new DateTime());
+
+//        $post = $posts->find(14);
+//        $comment = $post->getComments()[0];
+//        $comment->setPost(null);
+//        $comments->add($comment, true);
+
+        //$post->removeComment($comment);
+        //$posts->add($post, true);
+
+//        $comment = new Comment();
+//        $comment->setText('Hello');
+//        $comment->setPost($post);
+//        $comment = $post->getComments()[0];
+//        $posts->add($post, true);
+//        $comments->add($comment, true);
+
 //        $user = new User();
 //        $user->setEmail('email@email.com');
 //        $user->setPassword('12345678');
