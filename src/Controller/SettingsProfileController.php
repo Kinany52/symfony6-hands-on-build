c66e551fe64c5ec06ea039b1b0c42164290ea851
Sarmad Al Kinany <sarmad.alkinany@tivian.com>
2024-08-26 13:15:24 +0200

    embedding forms

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,15 +5,21 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
-    public function profile(Request $request): Response
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(
+        Request $request,
+        UserRepository $users
+    ): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -23,10 +29,17 @@ class SettingsProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userProfile = $form->getData();
             // Save this somehow
             // Add the flash message
             // Redirect
+            $userProfile = $form->getData();
+            $user->setUserProfile($userProfile);
+            $users->add($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile settings were saved.'
+            );
+            return $this->redirectToRoute('app_settings_profile');
         }
 
         return $this->render('settings_profile/profile.html.twig', [
